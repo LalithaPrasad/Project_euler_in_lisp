9976cfb0b8ffeb8bb2a165b45bdc7504f1ac9f69
Lalitha Prasad <lalithaprasad@gmail.com>
2025-01-21 17:44:21 +0530

    Add files via upload

---
+++ project_euler_47.lisp
@@ -0,0 +1,17 @@
+(defun dpf (n)
+  (labels ((helper (n x pl)
+				   (cond
+					 ((< n x) pl)
+					 ((zerop (rem n x)) (helper (floor n x) x (if (member x pl) pl (cons x pl))))
+					 (t (helper n (+ x 1) pl)))))
+	(helper n 2 '())))
+
+(defun euler-47 (n)
+  (cond
+	((/= 4 (length (dpf n))) (euler-47 (+ n 1)))
+	((/= 4 (length (dpf (+ n 1)))) (euler-47 (+ n 2)))
+	((/= 4 (length (dpf (+ n 2)))) (euler-47 (+ n 3)))
+	((/= 4 (length (dpf (+ n 3)))) (euler-47 (+ n 4)))
+	(t n)))
+
+(format t "~A~%" (euler-47 2))
